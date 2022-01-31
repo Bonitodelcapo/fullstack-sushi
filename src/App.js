771ccce1e95ff7b7663909ae7f64a462cc0fdd82
Bonitodelcapo <bonifazigianluca2@gmail.com>
2022-01-31 03:59:59 +0100

    class Card and state in App.js

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+
+import Navbar from './components/navbar';
+import Card from './components/card';
+
+import california from './images/california.png';
+import dragon from './images/dragon.png';
+import dynamite from './images/dynamite.png';
+import philadelphia from './images/philadelphia.png';
+import rainbow from './images/rainbow.png';
+import shrimp from './images/shrimp.png';
+
+class App extends Component {
+  state = {
+    cards: [
+      {id : 0, nome: "California", immagine: california, prezzo: 1.99},
+      {id : 1, nome: "Dragon", immagine: dragon, prezzo: 1.99},
+      {id : 2, nome: "Dynamite", immagine: dynamite, prezzo: 1.99},
+      {id : 3, nome: "Philadelphia", immagine: philadelphia, prezzo: 1.99},
+      {id : 4, nome: "Rainbow", immagine: rainbow, prezzo: 1.99},
+      {id : 5, nome: "Shrimp", immagine: shrimp, prezzo: 1.99}
+
+    ]
+  }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className='container'>
+          <h1>Cosa desideri ordinare?</h1>
+          <hr />
+          <div className='row'>
+            {this.state.cards.map(card => (
+              <Card
+                key={card.id}
+                nome={card.nome}
+                prezzo={card.prezzo}
+                immagine={card.immagine} />
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
+
+
+// riga 35-38: a sinistra sono referenziati i props come definiti in card.js. a destra {card.props}
+// abbiamo preso come riferimento la lista 'cards' contenuto nello state attuale. Per ogni oggetto card 
+// presente in questa lista abbiamo creato un componente React "Card".
+// poi abbiamo assegnato ai suoi prop (del component Card in card.js) i relativi valori dell'oggetto card (definito nella lista)
---
+++ src/components/card.js
@@ -0,0 +1,31 @@
+import {Component} from 'react'
+import App from '../App';
+
+class Card extends Component {
+    render () {
+        return(
+            <div className="col">
+                <div className="card" style={{ width: '18rem', textAlign: 'center'}}>
+                  <img src= {this.props.immagine} className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">{this.props.nome} Roll</h5>
+                    <p className="card-text">€{this.props.prezzo}</p>
+                    <button href="#" className="btn btn-outline-danger">Elimina</button>
+                  </div>
+                </div> 
+            </div>
+        );
+    }
+}
+
+export default Card;
+
+// abbiamo cambiato l'oggetto card da function() a class perche solo in questo modo si possono passare dei parametri 
+// per renderlo un oggetto riutilizzabile al 100%
+// per poterlo fare abbiamo cambiato anche App.js da una function ad una class 
+
+// props sono gli attributi che vengono passati da un componente padre ad un componente figlio
+// this è semplicemente un prefisso che identifica l'istanza del componente Card 
+
+
+
